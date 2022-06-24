36ec11f682a7c2a6015933e44527f21a6c14c27c
FernandoJuarez24 <57969849+FernandoJuarez24@users.noreply.github.com>
2022-06-24 16:56:01 -0500

    Delete abajo.js

--- abajo.js
+++
@@ -1,7 +0,0 @@
-/*Boton Abajo*/
-let abajo = document.querySelector('#abajo');
-
-abajo.addEventListener('click', () => {
-  document.body.scrollTop = 900;
-  document.documentElement.scrollTop = 900;
-});
